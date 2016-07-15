80c58d415fa69e55f1733ce0ea3af183f979dea4
Corey <corey@coreypon.com>
2016-07-15 18:52:33 -0500

    Adding the models for the api

---
+++ SamplesApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SamplesApi.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
